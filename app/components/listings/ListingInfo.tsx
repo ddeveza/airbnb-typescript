803b6df13827267c3b358a3f450a1e26de745358
ddeveza <ddeveza@ymail.com>
2023-09-14 08:34:15 +0800

    forma code

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,56 +1,61 @@
-'use client'
+/** @format */
+
+"use client";
 
 import useCountries from "@/app/hooks/userCountries";
-import { User } from "@prisma/client"
+import { User } from "@prisma/client";
 import { IconType } from "react-icons";
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
 import dynamic from "next/dynamic";
 
-const Map = dynamic(()=> import('../Map'),{ssr:false})
+const Map = dynamic(() => import("../Map"), { ssr: false });
 
 interface ListingInfoProps {
-    user: User;
-    category?: {
-        icon: IconType,
-        label:string;
-        description:string;
-    };
-    description:string;
-    roomCount:number;
-    guestCount:number;
-    bathroomCount:number;
-    locationValue:string;
+      user: User;
+      category?: {
+            icon: IconType;
+            label: string;
+            description: string;
+      };
+      description: string;
+      roomCount: number;
+      guestCount: number;
+      bathroomCount: number;
+      locationValue: string;
 }
 
-const ListingInfo :React.FC<ListingInfoProps>= ({
-    bathroomCount,category,description,guestCount,locationValue,roomCount,user
-}) => {
-  const {getByValue} = useCountries();
-  const coordinates = getByValue(locationValue)?.latlng;
-  
-  return (
-    <div className="col-span-4 flex flex-col gap-8">
-      <div className="flex flex-col gap-2">
-        <div className="text-xl font-semibold flex flex-row items-center gap-2">
-            <div>Host by {user.name}</div>
-            <Avatar src={user.image} />
-        </div>
-        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
-          <div>{guestCount} guests</div>
-          <div>{roomCount} rooms</div>
-          <div>{bathroomCount} bathrooms</div>
-         
-        </div>
-      </div>
-      <hr/>
-      {category && (<ListingCategory icon={category.icon} label={category.label} description={category.description}/>)}
-      <hr/>
-      <div className="text-lg font-light text-neutral-500">{description}</div>
-      <hr/>
-      <Map center={coordinates}/>
-    </div>
-  )
-}
+const ListingInfo: React.FC<ListingInfoProps> = ({ bathroomCount, category, description, guestCount, locationValue, roomCount, user }) => {
+      const { getByValue } = useCountries();
+      const coordinates = getByValue(locationValue)?.latlng;
 
-export default ListingInfo
+      return (
+            <div className='col-span-4 flex flex-col gap-8'>
+                  <div className='flex flex-col gap-2'>
+                        <div className='text-xl font-semibold flex flex-row items-center gap-2'>
+                              <div>Host by {user.name}</div>
+                              <Avatar src={user.image} />
+                        </div>
+                        <div className='flex flex-row items-center gap-4 font-light text-neutral-500'>
+                              <div>{guestCount} guests</div>
+                              <div>{roomCount} rooms</div>
+                              <div>{bathroomCount} bathrooms</div>
+                        </div>
+                  </div>
+                  <hr />
+                  {category && (
+                        <ListingCategory
+                              icon={category.icon}
+                              label={category.label}
+                              description={category.description}
+                        />
+                  )}
+                  <hr />
+                  <div className='text-lg font-light text-neutral-500'>{description}</div>
+                  <hr />
+                  <Map center={coordinates} />
+            </div>
+      );
+};
+
+export default ListingInfo;
