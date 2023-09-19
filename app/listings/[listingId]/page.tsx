9f7e4354cde914069fd8c4a28675d26555e7a01e
ddeveza <ddeveza@ymail.com>
2023-09-19 10:37:46 +0800

    done the reservations functionality

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -5,6 +5,7 @@ import getListingById from "@/app/actions/getListingById";
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
       listingId?: string;
@@ -14,7 +15,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
       const listing = await getListingById({
             listingId: params.listingId,
       });
-
+      const reservations = await getReservations(params);
       const currentUser = await getCurrentUser();
 
       if (!listing) {
@@ -29,6 +30,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
             <ClientOnly>
                   <ListingClient
                         listing={listing}
+                        reservations={reservations}
                         currentUser={currentUser}
                   />
             </ClientOnly>
